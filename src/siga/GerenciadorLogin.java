27d1d0b33443b9df990fa376b3eb9e09443ef6ba
Posselinha <gabrielfilipeposs@hotmail.com>
2026-09-08 17:27:51 -0300

    refactor: Comentado e mantido o código do bloco condicional if/else á pedido do professor para comparação, começado a utilizar um atributo contendo uma instancia da simple factory FabricaPainel e realizado a utilizaçaõ do seu método criar para retornar a instancia do objeto de painel pertencente ao tipo de usuário informado

--- src/siga/GerenciadorLogin.java
+++ src/siga/GerenciadorLogin.java
@@ -2,22 +2,29 @@ package siga;
 
 public class GerenciadorLogin {
 
+    // Chamando instancia da classe FabricaPainel para utilizazr o simple factory
+    private final FabricaPainel fabrica = new FabricaPainel();
+
     public Painel montarPainel(String tipoUsuario) {
-        Painel painel;
 
         // Notado de que o bloco condicional abaixo está causando um forte acoplamento
         // para o metodo montarPainel, fazendo com que cada novo tipo de usuário que for
         // registrado futuramente fique dependente da modificação do 
         // bloco condicional para se obter um painel proprio.
-        if (tipoUsuario.equals("ALUNO")) {
-            painel = new PainelAluno();
-        } else if (tipoUsuario.equals("PROFESSOR")) {
-            painel = new PainelProfessor();
-        } else if (tipoUsuario.equals("COORDENADOR")) {
-            painel = new PainelCoordenador();
-        } else {
-            throw new IllegalArgumentException("Perfil desconhecido: " + tipoUsuario);
-        }
+        // Mantido o código morto á pedido do professor no intuito de comparação]
+        // Painel painel;
+        // if (tipoUsuario.equals("ALUNO")) {
+        //     painel = new PainelAluno();
+        // } else if (tipoUsuario.equals("PROFESSOR")) {
+        //     painel = new PainelProfessor();
+        // } else if (tipoUsuario.equals("COORDENADOR")) {
+        //     painel = new PainelCoordenador();
+        // } else {
+        //     throw new IllegalArgumentException("Perfil desconhecido: " + tipoUsuario);
+        // }
+        // Utilizando o metodo criar da fabrica para retornar o objeto do devido painel que será
+        // delegado ao usuário
+        Painel painel = fabrica.criar(tipoUsuario);
 
         painel.montar();
         return painel;
